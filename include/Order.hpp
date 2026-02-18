1844376a2dd150f3cc17d7cd05ca2b9902d22f2c
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-02-17 19:07:22 -0500

    feat: add Time in Force data type and member to Order struct

--- include/Order.hpp
+++ include/Order.hpp
@@ -10,14 +10,16 @@ struct Order
     Order* prev_ = nullptr;
     Quantity quantity_;
     Side side_;
+    TimeInForce tif_;
 
     Order() = delete;
 
-    Order(OrderID id, Price price, Quantity quantity, Side side)
+    Order(OrderID id, Price price, Quantity quantity, Side side, TimeInForce tif = TimeInForce::GTC)
         : id_{id}
         , price_{price}
         , quantity_{quantity}
         , side_{side}
+        , tif_{tif}
     {
     }
 };
--- include/Types.hpp
+++ include/Types.hpp
@@ -8,6 +8,13 @@ enum class Side : uint8_t
     Sell
 };
 
+enum class TimeInForce : uint8_t
+{
+    GTC,    // Good Till Canceled
+    IOC,    // Immediate or Canceled
+    FOK     // Fill or Kill
+};
+
 // Preferring int64_t to double for pricing numerical stability
 // Scaled by factor of 10000 (e.g. 1.2345 = 12345)
 using Price     = int64_t;
