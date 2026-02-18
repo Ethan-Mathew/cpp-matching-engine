14f46f32ffb2efe785345e3e413ad89ee8c2a44c
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-02-17 20:13:28 -0500

    feat: implement doubly linked-list owner class for various price levels

---
+++ include/PriceLevel.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "Order.hpp"
+#include "Types.hpp"
+
+#include <cstdint>
+
+class PriceLevel
+{
+public:
+    explicit PriceLevel(Price price)
+        : price_{price}
+    {
+    }
+
+    void addOrder(Order* order)
+    {
+        // Internal accounting modified by new order 
+        totalVolume_ += order->quantity_;
+        orderCount_++;
+        
+        if (!head_) // If linked-list is empty
+        {
+            head_ = order;
+            tail_ = order;
+            order->prev_ = nullptr;
+            order->next_ = nullptr;
+        }
+        else // Add to back of linked-list
+        {
+            order->prev_ = tail_;
+            order->next_ = nullptr;
+            tail_->next_ = order;
+            tail_ = order;
+        }
+    }
+
+    void removeOrder(Order* order)
+    {
+        totalVolume_ -= order->quantity_;
+        orderCount_--;
+
+        // Handle case where removed order is the linked-list head
+        if (order->prev_)
+        {
+            (order->prev_)->next_ = order->next_;
+        }
+        else
+        {
+            head_ = order->next_;
+        }
+
+        // Handle case where removed order is the linked-list tail
+        if (order->next_)
+        {
+            (order->next_)->prev_ = order->prev_;
+        }
+        else
+        {
+            tail_ = order->prev_;
+        }
+
+        order->prev_ = nullptr;
+        order->next_ = nullptr;
+    }
+
+private:
+    Price price_;
+    uint64_t totalVolume_ = 0;
+    uint32_t orderCount_  = 0;
+
+    Order* head_ = nullptr;
+    Order* tail_ = nullptr;
+};
